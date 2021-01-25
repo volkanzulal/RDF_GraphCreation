afeed88260e283fcdfb96fa75ad39b1f54b24733
Volkan Zulal <zulalvolkan@gmail.com>
2021-01-25 21:52:17 +0100

    Lumb1 size

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,24 +2,9 @@ import org.apache.jena.rdf.model.ModelFactory
 
 object Main extends App {
   val model = ModelFactory.createDefaultModel()
-  model.read("file:lumb1.ttl","TRIPLE")
+  model.read("file:lumb1.ttl","TTL")
 
   println("rdf dataset size ="+model.size())
-  val typeProperty = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+  val typeProperty = "https://www.w3.org/RDF/Validator/"
 
-
-  def typePropertySize() = {
-    val rdfType = model.createProperty(typeProperty)
-    val iterator = model.listSubjectsWithProperty(rdfType)
-    iterator.toList.size
-  }
-
-  def numberOfDrugs()= {
-    val rdfType = model.createProperty(typeProperty)
-    val obj = model.createResource("http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/drugs")
-    model.listSubjectsWithProperty(rdfType,obj).toList.size
-  }
-
-  println("number of triples with an rdf:type property =" + typePropertySize)
-  println("number of drugs ="+ numberOfDrugs)
 }
